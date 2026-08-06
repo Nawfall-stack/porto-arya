e26e74829d03903fdc7b1ba45812caacb1cb0fe0
nawfall-stack <saynaufal55@gmail.com>
2026-08-06 21:46:06 +0700

    changing the config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**", // Mengizinkan semua path gambar dari domain ini
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
