2e15c30d7344a07e7ae78039f3a86c9dbf70fed1
NAUFALIRFANSYAH1 <belajarmtkku1@gmail.com>
2026-08-15 22:40:46 +0700

    minor fix : lowercase string

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
-const inter = Inter({ subsets: ['Latin'] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Arya Team - Web Portofolio",
